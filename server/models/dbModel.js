636ab611c39cbaa3b02a814a1666cc631d55262e
bprhee01 <73865293+bprhee01@users.noreply.github.com>
2022-08-16 16:38:18 -0400

    init database

--- server/models/dbModel.js
+++ server/models/dbModel.js
@@ -1,5 +1,25 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('../secrets');
 
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'EkhoMS'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
 
+const Schema = mongoose.Schema;
 
+const resultSchema = new mongoose.Schema({
+    experimentName: ,
+    Context: ,
+    legacyResult: ,
+    microserviceResult: ,
+    legacyTime: ,
+    msTime: ,
+    mismatch: ,
 
-export default { };
+})
+const Results = mongoose.model('Results', resultSchema);
+export default { Results };

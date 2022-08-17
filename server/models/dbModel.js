e5d4da04dec49eae6f361097954c1990d9236a8a
bprhee01 <73865293+bprhee01@users.noreply.github.com>
2022-08-16 20:40:53 -0400

    schema changes

--- server/models/dbModel.js
+++ server/models/dbModel.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
-const { MONGO_URI } = require('../secrets');
-
+import mongoose from 'mongoose';
+//import { MONGO_URI } from '../secret';
+//import secret from '../secret'
+const MONGO_URI = 'mongodb+srv://ekho:MQmP3Zfzj5lT6Yd2@ekhoms.5w0kgxu.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -12,13 +13,30 @@ mongoose.connect(MONGO_URI, {
 const Schema = mongoose.Schema;
 
 const resultSchema = new mongoose.Schema({
-    experimentName: ,
-    Context: ,
-    legacyResult: ,
-    microserviceResult: ,
-    legacyTime: ,
-    msTime: ,
-    mismatch: ,
+    experimentName: {
+    type:String,
+    required: true
+  } ,
+    Context: {
+    type:Object,
+    required: true
+  },
+    results: {
+    type:Object,
+    required: true
+  },
+    legacyTime: {
+    type:String,
+    required: true
+  } ,
+    msTime: {
+    type:String,
+    required: true
+  } ,
+    mismatch: {
+    type:Boolean,
+    required: true
+  },
 
 })
 const Results = mongoose.model('Results', resultSchema);

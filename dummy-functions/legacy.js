991eab74db9e3c56d8e0b5e023c89c2afeb47027
awcheung1213 <awcheung1213@gmail.com>
2022-08-18 09:07:26 -0700

    completed dummy monolith/microservice. Added microservice trigger

--- dummy-functions/legacy.js
+++ dummy-functions/legacy.js
@@ -1,1 +1,17 @@
 // For legacy function logic
+
+const legacyFunctions = {};
+
+legacyFunctions.fizzBuzz = num => {
+    const arr = [];
+    for(let i = 1; i <= num; i++){
+        if(i % 5 === 0 && i % 3 === 0) arr.push('fizzbuzz');
+        else if(i % 5 === 0) arr.push('buzz')
+        else if(i % 3 === 0) arr.push('fizz')
+        else arr.push(i);
+    }
+    console.log(arr)
+    return arr;
+}
+
+module.exports = legacyFunctions;

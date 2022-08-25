6ed92791edc018fb584ec7684e0bd65e617ef02b
awcheung1213 <awcheung1213@gmail.com>
2022-08-25 09:41:30 -0700

    Presentation Version

--- samples/dummy-legacy/legacy.js
+++ samples/dummy-legacy/legacy.js
@@ -4,17 +4,25 @@ const { ekhojs }= require('ekho-js')
 const legacyFunctions = {};
 
 legacyFunctions.fizzBuzz = (input) => {
-    ekhojs.wrap()
-    return ekhojs.wrap((num) => {
-    const arr = [];
-    for(let i = 1; i <= num; i++){
-        if(i % 5 === 0 && i % 3 === 0) arr.push('fizzbuzz');
-        else if(i % 5 === 0) arr.push('buzz')
-        else if(i % 3 === 0) arr.push('fizz')
-        else arr.push(i);
-    }
-    console.log('fizzbuzzlegacy',arr)
-    return arr;
+    return ekhojs.wrap((array) => {
+      //Approach#1 TimeComplexity: O(n2) SpaceComplexity: O(n)
+  //declare an empty array as a return
+  const returnArr = [];
+  //iterate through the passed in array
+  for (let i = 0; i < array.length; i++){
+  //create a temp of passed in array
+  const tempArr = [...array];
+  //at each index splice the temp array at the current index 
+  tempArr.splice(i, 1);
+  //iterate through the temp array and multiply each element and push into the return array
+  let product = 1;
+  for (el of tempArr){
+    product *= el;
+  }
+  returnArr.push(product);
+  }
+  //return the return array
+  return returnArr;
 }, 'test', {type: 'test'}, 'http://localhost:443', input, {query: {'body': input}})
 }
 
